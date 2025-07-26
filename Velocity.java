6a937d3a63020c677b386db77e0c2b1a6f6bd615
KuChaZi <130812332+KuChaZi1337@users.noreply.github.com>
2025-07-26 17:57:47 +0800

    Add files via upload

---
+++ Velocity.java
@@ -0,0 +1,14 @@
+package dick.modules.combat;
+
+import an.Jni;
+import dick.Module;
+import dick.events.PacketReceiveEvent;
+
+@Jni
+public class Velocity extends Module {
+
+    @Listener
+    public void onReceive(PacketReceiveEvent event){
+        // 想吃迪克了？
+    }
+}
